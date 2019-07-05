0f817bfdeb207b27c8b394475e96ca62f602bc99
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-07-04 23:15:43 -0400

    added function to the start button, shows and displays the first question

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,37 @@
 //Define variables
-
-//Define functions
-
-//start button
-var startButtonDisplay = $("#button");
-var timeDisplay = $("#time");
-
-function questions(){
-    timeDisplay.text("Hello! It worked");
+var test = {
+    question1: {
+        question: "Who hides eggs on a Sunday?",
+        choices: ["Nesquik Bunny", "Bugs Bunny", "The Easter Bunny", "The White Rabbit"],
+        answer: "The Easter Bunny"
+    }
 }
 
+//HTML display variables
+var startButtonDisplay = $("#button");
+var timeDisplay = $("#time");
+var questionDisplay = $("#question");
+var answerDisplay = $("#answers");
+
+
+//Define functions
+//function to show the questions
+function questions(){
+    startButtonDisplay.text("");
+    timeDisplay.text("Time remaining left: ");
+    questionDisplay.text(test.question1.question);
+    for(var i=0; i<4; i++){
+        answerDisplay.append("<h5>" + test.question1.choices[i] + "</h5>");
+    }
+}
+
+//function to show the start button
 function start(){
     startButtonDisplay.html("<button type='button' class='btn btn-danger' id='start'>START</button>");
-    $("#start").on("click", function() {
+    $("#start").on("click", function(){
         questions();
     });
 }
 
-//Main sections
+//Main game sections
 start();

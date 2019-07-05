e65889cab11feec2524e172ad174a23fae31c2ec
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-07-05 00:10:02 -0400

    added answer function that will show whether the user answers the question correctly

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -13,24 +13,53 @@ var timeDisplay = $("#time");
 var questionDisplay = $("#question");
 var answerDisplay = $("#answers");
 
+//timer variables
+
+
 
 //Define functions
+//function to show the answer
+function answer(){
+
+    //if user guesses it right
+    // if()
+    startButtonDisplay.text("");
+    timeDisplay.text("Time remaining left: ");
+    questionDisplay.text("ANSWER TIME");
+    console.log(questionDisplay);
+    answerDisplay.text("");
+
+    //if user guesses it wrong
+
+}
+
 //function to show the questions
 function questions(){
+
+    //timer
+    var time = 10;
+    // setTimeout(answer, 1000*10);
+
+    //display of questions & answers
     startButtonDisplay.text("");
     timeDisplay.text("Time remaining left: ");
     questionDisplay.text(test.question1.question);
     for(var i=0; i<4; i++){
-        answerDisplay.append("<h5>" + test.question1.choices[i] + "</h5>");
+        answerDisplay.append("<h5 id=answer" + i + ">" + test.question1.choices[i] + "</h5>");
     }
+
+    //show the answer page
+    if(test.question1.answer == test.question1.choices[2]){
+        $("#answer2").on("click",answer);
+    }
+
+
 }
 
 //function to show the start button
 function start(){
     startButtonDisplay.html("<button type='button' class='btn btn-danger' id='start'>START</button>");
-    $("#start").on("click", function(){
-        questions();
-    });
+    $("#start").on("click", questions)
 }
 
 //Main game sections

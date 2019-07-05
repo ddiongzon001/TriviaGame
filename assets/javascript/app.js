11c9b7fe4208bdf6a94ae25ba1422fc2e3e9905f
Dealan Diongzon <dealan.diongzon@gmail.com>
2019-07-04 22:37:07 -0400

    added start button at the start and made sure the on click function worked

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,21 @@
-$('#test').text("hello!");
+//Define variables
+
+//Define functions
+
+//start button
+var startButtonDisplay = $("#button");
+var timeDisplay = $("#time");
+
+function questions(){
+    timeDisplay.text("Hello! It worked");
+}
+
+function start(){
+    startButtonDisplay.html("<button type='button' class='btn btn-danger' id='start'>START</button>");
+    $("#start").on("click", function() {
+        questions();
+    });
+}
+
+//Main sections
+start();
